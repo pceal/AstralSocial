3b4ba67f8fe0138fd0bd097650067d40a2666ff5
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-05 18:52:35 +0200

    Endpoint traer user por username e id OK y funcionando

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,9 +6,27 @@ const UserController = {
       const user = await User.create(req.body)
       res.status(201).send(user)
     } catch (error) {
-      res.status(500).send("Ha habido un problema al crear el usuari@")
+      res.status(500).send("Ha habido un problema al crear al usuari@")
+    }
+  },
+  async getUserByUsername(req, res) {
+    try {
+      const username = new RegExp(req.params.username, "i");
+      const users = await User.find({username});
+      res.status(200).send(users)
+    } catch (error) {
+      res.status(500).send("Ha habido un problema al buscar al usuari@")
+    }
+  },
+  async getUserById(req, res) {
+    try {
+      const user = await User.findById(req.params._id)
+      res.status(200).send(user)
+    } catch (error) {
+      res.status(500).send("Ha habido un problema al buscar al usuari@")
     }
   }
 }
+
 
 module.exports = UserController
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const UserController = require("../controllers/UserController")
 
 router.post("/", UserController.create)
+router.get("/username/:username", UserController.getUserByUsername)
+router.get("/id/:_id", UserController.getUserById)
 
 module.exports = router
 
