206962a427d9449b6dc1db52a8499ba0fb8e2ecf
pceal <patricia.cerezo@hotmail.com>
2025-06-09 12:41:16 +0200

    comments pat

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express")
+const { dbConnection } = require("./config/config")
 const app = express()
 const PORT = 8080
 
-
+dbConnection()
 // MIDDLEWARE
 app.use(express.json())
 
+//RUTAS
 
+
+
+app.use("/comments", require("./routes/comments"))
 
 // SERVER
 app.listen(PORT, () => {
